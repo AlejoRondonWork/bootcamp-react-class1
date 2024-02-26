1d1c4855307f9387c0ce615b34ea53a35328793e
David Alejandro Rondon Berrio <david.rondon@globant.com>
2024-02-26 12:33:28 -0500

    First Vite Project has been uploaded

---
+++ Homework_FirstViteProject/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <div className='overlay'></div>
+
+      <div className='text'>
+        <div className='wrapper'>
+          <div className='letter'>A</div>
+          <div className='shadow'>A</div>
+        </div>
+        <div className='wrapper'>
+          <div className='letter'>L</div>
+          <div className='shadow'>L</div>
+        </div>
+        <div className='wrapper'>
+          <div className='letter'>E</div>
+          <div className='shadow'>E</div>
+        </div>
+        <div className='wrapper'>
+          <div className='letter'>J</div>
+          <div className='shadow'>J</div>
+        </div>
+        <div className='wrapper'>
+          <div className='letter'>O</div>
+          <div className='shadow'>O</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
